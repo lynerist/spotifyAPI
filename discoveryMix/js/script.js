c6c96ff75c2b6feaf94080671edde845e6c8ec26
lynerist <leonardodarksoul@gmail.com>
2024-10-14 23:43:20 +0200

    scelte

--- discoveryMix/js/script.js
+++ discoveryMix/js/script.js
@@ -4,10 +4,13 @@ const waitTimeBeforeSearch = 1000
 var lastChangeTime = 0
 var lastQuery = ""
 
+var seed = {"artists":[],"tracks":[],"genres":[]}
+
 // ARTIST
 
 const inputFieldArtist = document.getElementById('artist-input');
 const dropdownArtist = document.getElementById('artist-results');
+let artists = []
 
 async function fetchSearchedArtist(query) {
     const result = await fetch(`https://api.spotify.com/v1/search?q=${encodeURI(query)}&type=artist`, {
@@ -20,18 +23,20 @@ function updateImage(src){
     document.getElementById("image-placeholder").src = src
 }
 
-async function updateSearchedArtist() {
-    if (Date.now() - lastChangeTime < waitTimeBeforeSearch){
-        setTimeout(updateSearchedArtist, waitTimeBeforeSearch)
-        return
-    }
-    lastChangeTime = Date.now()
-
+async function updateSearchedArtist(controls=true) {
     const query = inputFieldArtist.value.toLowerCase();
-    if (query == lastQuery){
-        return
+    if (controls){
+        if (Date.now() - lastChangeTime < waitTimeBeforeSearch){
+            setTimeout(updateSearchedArtist, waitTimeBeforeSearch)
+            return
+        }
+        lastChangeTime = Date.now()
+        
+        if (query == lastQuery){
+            return
+        }
+        lastQuery = query
     }
-    lastQuery = query
     
     // Clear previous results
     dropdownArtist.innerHTML = '';
@@ -41,17 +46,29 @@ async function updateSearchedArtist() {
     }
 
     let result = await fetchSearchedArtist(query)
-    console.log(result.artists)
-
-    for (let i=0; i<result.artists.limit; i++){
+    artists = result.artists.items.filter(artist => 
+        seed["artists"].filter(t => t["id"] === artist["id"]).length==0
+    );
+    
+    for (let i=0; i<artists.length; i++){
         const option = document.createElement('option');
         option.value = i;
-        option.textContent += result.artists.items[i].name;
+        option.textContent += artists[i].name;
             
-        option.onmouseover = (func  => updateImage(result.artists.items[i].images[1].url))
+        option.onmouseover = (func  => updateImage(artists[i].images[1].url))
         dropdownArtist.appendChild(option);
     }  
     dropdownArtist.style.display = 'block';
+}
+
+function addArtist(index){
+    if (seed.artists.length+seed.tracks.length+seed.genres.length>=5){
+        alert("You can't add anything")
+        return
+    }
+    seed["artists"].push(artists[index])
+    console.log(seed)
+    updateSearchedArtist(false)
 }
 
 // Listen for input changes
@@ -61,8 +78,7 @@ inputFieldArtist.addEventListener('input', updateSearchedArtist);
 dropdownArtist.addEventListener('change', function () {
     const selectedArtist = this.options[this.selectedIndex].text;
     if (selectedArtist !== 'No results found') {
-        alert('You selected: ' + selectedArtist);
-        // Further logic can be added here to handle the selected Artist
+        addArtist(this.selectedIndex)
     }
 });
 
@@ -70,6 +86,7 @@ dropdownArtist.addEventListener('change', function () {
 
 const inputFieldTrack = document.getElementById('song-input');
 const dropdownTrack = document.getElementById('song-results');
+let tracks = []
 
 async function fetchSearchedTrack(query) {
     const result = await fetch(`https://api.spotify.com/v1/search?q=${encodeURI(query)}&type=track`, {
@@ -78,34 +95,48 @@ async function fetchSearchedTrack(query) {
     return await result.json()
 }
 
-async function updateSearchedTrack() {
-    if (Date.now() - lastChangeTime <= waitTimeBeforeSearch){
-        setTimeout(updateSearchedTrack, waitTimeBeforeSearch)
-        return
-    }
-    lastChangeTime = Date.now()
-
+async function updateSearchedTrack(controls = true) {
     const query = inputFieldTrack.value.toLowerCase();
-    if (query == lastQuery){
-        return
+    if (controls){
+        if (Date.now() - lastChangeTime <= waitTimeBeforeSearch){
+            setTimeout(updateSearchedTrack, waitTimeBeforeSearch)
+            return
+        }
+        lastChangeTime = Date.now()
+        
+        if (query == lastQuery){
+            return
+        }
+        lastQuery = query
     }
-    lastQuery = query
     
     // Clear previous results
     dropdownTrack.innerHTML = '';
 
     let result = await fetchSearchedTrack(query)
-    console.log(result.tracks)
+    tracks = result.tracks.items.filter(track => 
+        seed["tracks"].filter(t => t["id"] === track["id"]).length==0
+    );
 
-    for (let i=0; i<result.tracks.limit; i++){
+    for (let i=0; i<tracks.length; i++){
         const option = document.createElement('option');
         option.value = i;
-        option.textContent += result.tracks.items[i].name;
+        option.textContent += tracks[i].name;
             
-        option.onmouseover = (func  => updateImage(result.tracks.items[i].album.images[1].url))
+        option.onmouseover = (func  => updateImage(tracks[i].album.images[1].url))
         dropdownTrack.appendChild(option);
     }  
     dropdownTrack.style.display = 'block';
+}
+
+function addTrack(index){
+    if (seed.artists.length+seed.tracks.length+seed.genres.length>=5){
+        alert("You can't add anything")
+        return
+    }
+    seed["tracks"].push(tracks[index])
+    console.log(seed)
+    updateSearchedTrack(false)
 }
 
 // Listen for input changes
@@ -113,42 +144,44 @@ inputFieldTrack.addEventListener('input', updateSearchedTrack);
 
 // Handle selection from dropdown
 dropdownTrack.addEventListener('change', function () {
-    const selectedSong = this.options[this.selectedIndex].text;
-    if (selectedSong !== 'No results found') {
-        alert('You selected: ' + selectedSong);
-        // Further logic can be added here to handle the selected song
+    const selectedTrack = this.options[this.selectedIndex].text;
+    if (selectedTrack !== 'No results found') {
+        addTrack(this.selectedIndex)
     }
 });
-
 
 // GENRE
 
 const inputFieldGenre = document.getElementById('genre-input');
 const dropdownGenre = document.getElementById('genre-results');
+var filteredGenres = []
 
 const genres = await fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
     method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
 }).then((result) => result.json().then((res) => res.genres))
 
-function updateSearchedGenre() {
-    if (Date.now() - lastChangeTime <= waitTimeBeforeSearch){
-        setTimeout(updateSearchedGenre, waitTimeBeforeSearch)
-        return
-    }
-    lastChangeTime = Date.now()
-
+function updateSearchedGenre(controls=true) {
     const query = inputFieldGenre.value.toLowerCase();
-    if (query == lastQuery){
-        return
+    
+    if (controls){
+        if (Date.now() - lastChangeTime <= waitTimeBeforeSearch){
+            setTimeout(updateSearchedGenre, waitTimeBeforeSearch)
+            return
+        }
+        lastChangeTime = Date.now()
+    
+        if (query == lastQuery){
+            return
+        }
+        lastQuery = query
     }
-    lastQuery = query
 
     // Clear previous results
     dropdownGenre.innerHTML = '';
 
-    // Filter songs based on the query
-    const filteredGenres = genres.filter(genre => 
-        genre.toLowerCase().includes(query)
+    // Filter songs based on the query && remove selected genres
+    filteredGenres = genres.filter(genre => 
+        genre.toLowerCase().includes(query) && ! seed["genres"].includes(genre)
     );
     
     if (filteredGenres.length > 0) {
@@ -178,8 +211,16 @@ inputFieldGenre.addEventListener('input', updateSearchedGenre);
 dropdownGenre.addEventListener('change', function () {
     const selectedGenre = this.options[this.selectedIndex].text;
     if (selectedGenre !== 'No results found') {
-        alert('You selected: ' + selectedGenre);
-        // Further logic can be added here to handle the selected Genre
+        addGenre(this.selectedIndex)
     }
 });
 
+function addGenre(index){
+    if (seed.artists.length+seed.tracks.length+seed.genres.length>=5){
+        alert("You can't add anything")
+        return
+    }
+    seed["genres"].push(filteredGenres[index])
+    console.log(seed)
+    updateSearchedGenre(false)
+}
